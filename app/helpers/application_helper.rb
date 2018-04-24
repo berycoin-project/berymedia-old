eb30accfb033ba8b827a86414627a0d28d2a91eb
Bilal Haider <ronald.n.mcatee@gmail.com>
2018-04-24 20:04:23 +0500

    statuses calc moved to application controlelr

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,23 @@ module ApplicationHelper
   def is_member?
     current_user.roles.where(name: "member").count == 1
   end
+
+  def newStatus
+    Status.find_by_name("new")
+  end
+  def approvedStatus
+    Status.find_by_name("approved")
+  end
+  def rejectedStatus
+    Status.find_by_name("rejected")
+  end
+  def activeStatus
+    Status.find_by_name("active")
+  end
+  def completeStatus
+    Status.find_by_name("completed")
+  end
+  def cancelStatus
+    Status.find_by_name("cancelled")
+  end
 end
--- app/helpers/statuses_helper.rb
+++ app/helpers/statuses_helper.rb
@@ -1,20 +1,3 @@
 module StatusesHelper
-  def newStatus
-    Status.find_by_name("new")
-  end
-  def approvedStatus
-    Status.find_by_name("approved")
-  end
-  def rejectedStatus
-    Status.find_by_name("rejected")
-  end
-  def activeStatus
-    Status.find_by_name("active")
-  end
-  def completeStatus
-    Status.find_by_name("completed")
-  end
-  def cancelStatus
-    Status.find_by_name("cancelled")
-  end
+
 end
